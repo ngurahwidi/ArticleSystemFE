ba1174cbc8cff537bc380b37208e97b5f3aea8b6
ngurahwidi <widi050917@gmail.com>
2024-10-28 08:34:04 +0800

    "Dashboard page finishing"

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,3 +1,7 @@
+export const BtnPrimary = ({children, className = 'btn btn-primary btn-sm rounded'}) => {
+    return <button className={className}>{children}</button>
+}
+
 export const BtnWarning = ({children, className="btn btn-warning btn-sm w-100 rounded px-4 py-2"}) => {
     return <button className={className}>{children}</button>
 }
--- src/page/Article.jsx
+++ src/page/Article.jsx
@@ -1,9 +1,35 @@
+import logo from '../assets/image/avatar.svg'
+import {BtnPrimary} from "../component/Button.jsx";
+
 const Article = () => {
     return (
         <>
             <div>
                 <h1>Hello Widi</h1>
                 <p>This is article page</p>
+                <table className="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Feature Image</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td><img src={logo}/></td>
+                        <td>Blog Pemrograman Javascript</td>
+                        <td>Blog yang membahas tentang dasar-dasar pemrograman dengan menggunakan bahasa javascript</td>
+                        <td>
+                            <BtnPrimary>Tambah</BtnPrimary>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     )
